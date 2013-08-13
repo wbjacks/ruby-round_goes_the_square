6fdacba4c79c026e527d7967edb8c93251020982
Will Jackson <greenerside71@gmail.com>
2013-08-12 23:48:03 -0700

    I wrote more things but then vim started typing in like greek or something so I stopped and deleted everything

--- classes.rb
+++ classes.rb
@@ -1,5 +1,6 @@
-class Square
+# THIS IS THE ONLY FUCKING FILE K DEAL WITH IT
 
+class Square
     def initialize 
         
 
@@ -14,6 +15,14 @@ class CoordConverter:
         @abs_coords = args.get(:abs_coords, nil)
         @sq_coords = args.get(:sq_coords, nil)
 
-        
-    end
+        unless @abs_coords.nil? and @sq_coords.nil?:
+            if not @abs_coords.nil? and not @sq_coords.nil?:
+                pix_per_square *               
+
+            if @abs_coords.nil?:
+                
+
+            end
+        end
+     end
 end
