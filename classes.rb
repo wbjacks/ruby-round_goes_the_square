59aab24f90002146c7e1bc58d47f9f9f1a17cdc5
Will Jackson <greenerside71@gmail.com>
2013-08-12 23:03:11 -0700

    struggz

---
+++ classes.rb
@@ -0,0 +1,19 @@
+class Square
+
+    def initialize 
+        
+
+    end
+
+end
+
+class CoordConverter:
+    attr_accessor :abs_coords
+
+    def initialize
+        @abs_coords = args.get(:abs_coords, nil)
+        @sq_coords = args.get(:sq_coords, nil)
+
+        
+    end
+end
